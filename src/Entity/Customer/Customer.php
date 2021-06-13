eb78a09d9e8e23b4818729c71c9add4adbb62259
Mateusz Zalewski <zaleslaw@gmail.com>
2021-06-13 17:11:33 +0200

    Custom field in endpoint

--- src/Entity/Customer/Customer.php
+++ src/Entity/Customer/Customer.php
@@ -6,6 +6,7 @@ namespace App\Entity\Customer;
 
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Customer as BaseCustomer;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity
@@ -13,4 +14,20 @@ use Sylius\Component\Core\Model\Customer as BaseCustomer;
  */
 class Customer extends BaseCustomer
 {
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", name="vat_number", nullable=true)
+     * @Groups({"admin:customer:read"})
+     */
+    private $vatNumber;
+
+    public function getVatNumber(): ?string
+    {
+        return $this->vatNumber;
+    }
+
+    public function setVatNumber(?string $vatNumber): void
+    {
+        $this->vatNumber = $vatNumber;
+    }
 }
